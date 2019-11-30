b9ef77cd7ccd264e5e0e28e598f9272018a7fa6e
charlinox <charles.casassus@gmail.com>
2019-11-30 01:49:10 +0100

    probleme with article ok

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -5,30 +5,30 @@ function htmlElements(responseText) {
     document.body.style.cursor = "default";
     
     // create a response container
-    var data = JSON.parse(responseText);
-    var responses = document.querySelector("#responses");
-    var response = document.createElement("div");
+    let data = JSON.parse(responseText);
+    let responses = document.querySelector("#responses");
+    let response = document.createElement("div");
     response.classList.add("response");
     responses.appendChild(response);
 
     // create a response
-    response = document.querySelector(".response");
-    p = document.createElement("p");
+    let answer = document.querySelector(".response");
+    let p = document.createElement("p");
     p.classList.add("grandpy-answer");
     p.textContent = data["grandpy_answer"] + data["address"];
-    response.appendChild(p);
+    answer.appendChild(p);
 
     // create a response google map and marker
-    var map = document.createElement("div");
+    let map = document.createElement("div");
     map.classList.add("map");
-    var mapObject = new google.maps.Map(map, {
+    let mapObject = new google.maps.Map(map, {
         center: {
             lat: data["lat"], 
             lng: data["lng"]
         },
         zoom: 8
     });
-    var markerObject = new google.maps.Marker({
+    let markerObject = new google.maps.Marker({
         position: {
             lat: data["lat"],
             lng: data["lng"]
@@ -38,15 +38,15 @@ function htmlElements(responseText) {
     response.appendChild(map);
     
     // create a response google map and marker
-    article = document.createElement("article");
+    let article = document.createElement("article");
     article.classList.add("wikipedia_article");
     if (data["error"] == false) {
-        p.textContent = data["grandpy_answer2"] + data['extract'];
+        article.textContent = data["grandpy_answer2"] + data['extract'] + "ZZZZZzzzz  ";
     }
     response.appendChild(article);
 
     // create a response link to wikipedia article
-    a = document.createElement("a");
+    let a = document.createElement("a");
     a.href = data['fullurl'];
     a.textContent = "En savoir plus";
     article.appendChild(a);
@@ -55,10 +55,10 @@ function htmlElements(responseText) {
     response.scrollIntoView();
 }
 
-var form = document.querySelector("form");
+let form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    var data = new FormData(form);
+    let data = new FormData(form);
     ajaxPost("/api", data, htmlElements);
     document.body.style.cursor = "wait";
 });
